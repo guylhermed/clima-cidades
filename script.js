0465411bbdf5b8bbeec1af600f93bb56f1e1c07f
Guylherme Duarte <guylherme_duarte@hotmail.com>
2023-01-02 18:43:51 -0300

    Finalizado CSS

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-// f760c1c59c3eb3c980d9de07f17f7f36
+const apiKey = "f760c1c59c3eb3c980d9de07f17f7f36";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input")
+const searchBtn = document.querySelector("#search");
